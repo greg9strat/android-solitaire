036a9beaf15b97d7d720092c134fe0888e9435eb
Greg Stratton <greg9strat@gmail.com>
2018-07-29 08:28:25 -0400

    Removed dex option per compiler warning; Added images for card suits (temporary);

--- MySolitaire/app/src/main/java/com/gstratton/games/mysolitaire/models/games/PlayKlondikeActivity.java
+++ MySolitaire/app/src/main/java/com/gstratton/games/mysolitaire/models/games/PlayKlondikeActivity.java
@@ -1,4 +1,4 @@
-package com.gstratton.games.mysolitaire;
+package com.gstratton.games.mysolitaire.models.games;
 
 import android.annotation.SuppressLint;
 import android.support.v7.app.ActionBar;
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+
+import com.gstratton.games.mysolitaire.R;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
